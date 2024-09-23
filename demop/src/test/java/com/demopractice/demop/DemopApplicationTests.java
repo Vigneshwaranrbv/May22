0a5bf978832c5d4658dc1655de7861b9fc6bb2fd
Vigneshwaranrbv <bvigneshwaran918@gmail.com>
2024-09-23 15:09:15 +0530

    Initial commit.

---
+++ demop/src/test/java/com/demopractice/demop/DemopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demopractice.demop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
